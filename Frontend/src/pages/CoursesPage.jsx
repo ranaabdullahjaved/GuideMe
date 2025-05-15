610cf6135672c080d4c43c5c1c691bd3c7de916f
ranaabdullahjaved <ranaabdullahjaveed2003@gmail.com>
2025-05-16 02:48:52 +0500

    Vercel fix 2

--- Frontend/src/pages/CoursesPage.jsx
+++ Frontend/src/pages/CoursesPage.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import CourseList from "../components/CoursesList";
 import NewCourseModal from "../components/NewCoursesModelPage";
 import "../styles/courses.css";
